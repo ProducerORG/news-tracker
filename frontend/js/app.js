a9f414874732c04d1e414126e840b435730de988
linus-producer <linus@producer.works>
2025-07-21 10:25:28 +0000

    datumsformat

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,9 +8,7 @@ const formatDate = (isoString) => {
     return date.toLocaleDateString('de-DE', {
         year: 'numeric',
         month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit'
+        day: '2-digit'
     });
 };
 
